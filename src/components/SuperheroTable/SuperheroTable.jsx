19296b0e01a32ff45ca97293d675dcef9f0a9010
andrew morales <andrew.morales725@gmail.com>
2021-11-15 17:30:42 -0500

    Corrected console warnings

--- src/components/SuperheroTable/SuperheroTable.jsx
+++ src/components/SuperheroTable/SuperheroTable.jsx
@@ -2,26 +2,30 @@ function SuperheroTable(props) {
     return ( 
         <div>
             <table>
+                <thead>
                 <tr>
                 <th>Name</th>
                 <th>Primary Ability</th>
                 <th>Secondary Ability</th>
                 </tr>
-                <tr>
-                {props.heroTable.map(superheroes => (
-                    <td key={superheroes.Id}>{superheroes.name}</td>
-                ))}
-                </tr>
-                <tr>
-                {props.heroTable.map(superheroes => (
-                    <td key={superheroes.Id}>{superheroes.primaryAbility}</td>
-                ))}
-                </tr>
-                <tr>
-                {props.heroTable.map(superheroes => (
-                    <td key={superheroes.Id}>{superheroes.secondaryAbility}</td>
-                ))}
-                </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    {props.heroTable.map(superheroes => (
+                        <td key={superheroes.superheroId}>{superheroes.name}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    {props.heroTable.map(superheroes => (
+                        <td key={superheroes.superheroId}>{superheroes.primaryAbility}</td>
+                    ))}
+                    </tr>
+                    <tr>
+                    {props.heroTable.map(superheroes => (
+                        <td key={superheroes.superheroId}>{superheroes.secondaryAbility}</td>
+                    ))}
+                    </tr>
+                </tbody>
             </table>
         </div>
      );
